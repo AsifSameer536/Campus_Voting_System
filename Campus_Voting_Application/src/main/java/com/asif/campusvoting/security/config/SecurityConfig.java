6292d9891b51456ab46113ade0ae4de3f8691b63
Asif Sameer <asifsameer536@gmail.com>
2026-07-05 00:36:02 +0530

    Implement CustomUserDetailsService

--- Campus_Voting_Application/src/main/java/com/asif/campusvoting/security/config/SecurityConfig.java
+++ Campus_Voting_Application/src/main/java/com/asif/campusvoting/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.asif.campusvoting.security;
+package com.asif.campusvoting.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
